02c1ac2dad62fd5d3f12d675601448679e8e3a0a
Timo Reents <timo.reents1@gmail.com>
2025-08-14 14:14:30 +0200

    Merlin7 scheduler

---
+++ src/slurm_scheduler_merlin7/schedulers/slurm_merlin7.py
@@ -0,0 +1,14 @@
+from aiida.schedulers.plugins.slurm import SlurmScheduler
+
+
+class CustomSlurmScheduler(SlurmScheduler):
+    
+    def _get_joblist_command(self, jobs=None, user=None):
+        """The command to report full information on existing jobs.
+
+        Separate the fields with the _field_separator string order:
+        jobnum, state, walltime, queue[=partition], user, numnodes, numcores, title
+        """
+        command = super()._get_joblist_command(jobs=jobs, user=user)
+        command.append('--clusters=all')
+        return command
